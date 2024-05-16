7e74f7387b64755a9d5ed2435d797bb69db7792f
Bradut Stanciu <danstanciu2002@gmail.com>
2024-05-16 14:30:42 +0200

    Loading diagram

--- sizing_tools/hinge_loading.py
+++ sizing_tools/hinge_loading.py
@@ -11,8 +11,8 @@ from mass_model.propulsion_system import PropulsionSystemMassModel
 from data.concept_parameters.concepts import concept_C1_5, concept_C2_1, concept_C2_6, concept_C2_10
 from mass_model.total import concept_iteration
 
-
-def L(eta: float = 0) -> tuple[float]:
+#todo : After midterm make sure to implement engine position on wing in code;
+def L(concept,eta: float = 0) -> tuple[float]:
     """_summary_
 
     Args:
@@ -21,18 +21,17 @@ def L(eta: float = 0) -> tuple[float]:
     Returns:
         tuple[float]: _description_
     """
-    model = TotalModel(concept_C1_5, initial_total_mass=1500)
+    model = TotalModel(concept, initial_total_mass=1500)
     mass_breakdown = model.mass_breakdown()
     print(mass_breakdown)
 
     n_design = model.aircraft.design_load_factor  # [-]
     M_fus = mass_breakdown['total'] - mass_breakdown['airframe'][
-        'wing'] - mass_breakdown['propulsion']['total']  # [kg]
+        'wing'] - mass_breakdown['propulsion']['total']/model.aircraft.motor_prop_count*(model.aircraft.motor_prop_count-model.aircraft.motor_wing_count) # [kg]
 
     print(M_fus)
     b = np.sqrt(model.aircraft.wing.area * model.aircraft.wing.aspect_ratio)
-    lmbd = 0.4  #TODO: remove hardcoding
-
+    lmbd = model.aircraft.taper
     V = n_design * M_fus * 9.81 / b * 2 / (1 +
                                            lmbd) * b / 2 * (1 - eta +
                                                             (lmbd - 1) * 0.5 *
@@ -40,11 +39,10 @@ def L(eta: float = 0) -> tuple[float]:
     M = n_design * M_fus * 9.81 / b * 2 / (1 + lmbd) * (b / 2)**2 * (
         1 - eta - 0.5 * (1 - eta**2) + (lmbd - 1) * 0.5 * (1 - eta - 1 / 3 *
                                                            (1 - eta**3)))
-
     return V, M  #N and Nm
 
 
-def W_engine(eta: np.ndarray = 0) -> tuple[float]:
+def W_engine(concept,eta: np.ndarray = 0) -> tuple[float]:
     """_summary_
 
     Args:
@@ -53,11 +51,11 @@ def W_engine(eta: np.ndarray = 0) -> tuple[float]:
     Returns:
         tuple[float]: _description_
     """
-    model = TotalModel(concept_C1_5, initial_total_mass=1500)
+    model = TotalModel(concept, initial_total_mass=1500)
     mass_breakdown = model.mass_breakdown()
 
     M_engine = mass_breakdown['propulsion'][
-        'total'] / 4  #TODO: remove hardcoding
+        'total'] / model.aircraft.motor_prop_count #assume same
     l_1 = 0.3
     l_2 = 0.5
 
@@ -75,14 +73,15 @@ def W_engine(eta: np.ndarray = 0) -> tuple[float]:
 
     V = V1 + V2 + V3
     M = M1 + M2 + M3
+    #assumption :engine contribution small so neglected. Hinge mechanism after engine so no influence on shear
 
     return V, M
 
 
-def get_load(eta):
+def get_load(concept,eta):
     eta = np.array([eta])
-    V_L, M_L = L(eta)
-    V_E, M_E = W_engine(eta)
+    V_L, M_L = L(concept,eta)
+    V_E, M_E = W_engine(concept,eta)
     return V_L + V_E, M_L + M_E
 
 
@@ -91,8 +90,12 @@ if __name__ == '__main__':
     from matplotlib import pyplot as plt
 
     eta = np.linspace(0, 1, 100)
-    plt.plot(eta, L(eta)[0] + W_engine(eta)[0], label='Shear')
-    plt.plot(eta, L(eta)[1] + W_engine(eta)[1], label='Moment')
+    concept=concept_C1_5
+    plt.plot(eta, L(concept,eta)[0] + W_engine(concept,eta)[0], label='Shear')
+    plt.plot(eta, L(concept,eta)[1] + W_engine(concept,eta)[1], label='Moment')
     plt.legend()
-    print(get_load(0.5))
+    print(get_load(concept,0.6))
+    print(get_load(concept_C2_1,0.6))
+    print(get_load(concept_C2_6),0.6)
+    print(get_load(concept_C2_10,0.6))
     plt.show()
