de9c3466f75ce4be96c206fd3f337888f16c9b28
Philip2628 <101248099+Philip2628@users.noreply.github.com>
2024-05-16 09:20:51 +0200

    small Changes

--- sizing_tools/wing_loading.py
+++ sizing_tools/wing_loading.py
@@ -30,11 +30,11 @@ class WingLoading:
         self.power_setting = power_setting
         self.mtow_setting = mtow_setting
         self._check_input()
-        self.V_stall = 31.3889  #m/s CS23 stall speed
-        self.ROC_ver = 5  # vertical rate of climb
-        self.Fom = 0.75  # figure of merit
-        self.n_prop = 0.8  #propeller efficiency
-        self.ROC_std = 3.65  #m/s from joby s4
+        self.V_stall = 31.3889               #m/s CS23 stall speed
+        self.ROC_ver = 5                     # vertical rate of climb
+        self.Fom = 0.75                      # figure of merit
+        self.n_prop = 0.8                    # propeller efficiency
+        self.ROC_std = 3.65                  # m/s from joby s4
         self.cd_cl_three_over_2 = 1 / 18
 
     def _check_input(self):
@@ -54,16 +54,6 @@ class WingLoading:
 
         logger.info(f'{W_over_S_opt=}')
         return W_over_S_opt
-
-    # def W_over_P_takeoff(self, a) -> float:
-    #     obj = EnergySystemMassModel(a, 1500)
-    #     obj._power()
-    #     print(obj.P_hv)
-    #     rho = Atmosphere(altitude=phase.ending_altitude).density()
-    #     rotor_disk_thrust = self.initial_total_mass * g
-    #     rotor_disk_area = 2 * math.pi * self.aircraft.propeller_radius**2
-    #     P_hv = rotor_disk_thrust**(3 / 2) / (
-    #         self.aircraft.figure_of_merit * np.sqrt(2 * rho * rotor_disk_area))
 
     def _wp(self, ws: np.ndarray) -> np.ndarray:
         ws = self.mtow_setting * ws
@@ -97,17 +87,11 @@ class WingLoading:
         if W_over_S_opt is not None:
 
             plt.axvline(x=self.w_s_stall_speed(), label=' Stall Speed')
-        # plt.axhline(y=W_over_P_vert_takeoff,
-        #             color='r',
-        #             linestyle='-',
-        #             label='Vertical TO requirement')
         plt.plot(xx, self._wp(xx), label='Cruise')
         plt.plot(xx, self.ver_climb(), label='Vertical Climb')
         plt.plot(xx, self.steady_climb(), label='Cruise Climb')
         plt.xlabel('W/S')
         plt.ylabel('W/P')
-        # plt.xlim(0, 2000)
-        # plt.ylim(0, 1)
         plt.legend()
         plt.show()
 
