304b207692eb36ef8537426fb022962e743c6898
ecerpac <ecerpac@student.tudelft.nl>
2024-05-29 14:55:36 +0200

    updated figure texts

--- utility/plotting/helper.py
+++ utility/plotting/helper.py
@@ -3,9 +3,9 @@ from utility.unit_conversion import convert_float
 
 def pct_func_mass(pct, allvalues: list[float]) -> str:
     mass = pct / 100. * sum(allvalues)
-    return "{:.1f}%\n({:.1f} kg)".format(pct, mass)
+    return "{:.0f}%\n({:.0f} kg)".format(pct, mass)
 
 
 def pct_func_energy(pct, allvalues: list[float]) -> str:
     energy = pct / 100. * convert_float(sum(allvalues), 'J', 'kWh')
-    return "{:.1f}%\n({:.1f} kWh)".format(pct, energy)
+    return "{:.0f}%\n({:.0f} kWh)".format(pct, energy)
