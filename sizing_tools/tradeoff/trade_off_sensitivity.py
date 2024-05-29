35e1fd7ce34836a3a0ebd489b90d0986424bb4d2
ecerpac <ecerpac@student.tudelft.nl>
2024-05-29 16:26:53 +0200

    updated trade-off sensitivity plot

--- sizing_tools/tradeoff/trade_off_sensitivity.py
+++ sizing_tools/tradeoff/trade_off_sensitivity.py
@@ -1,7 +1,8 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
+from utility.plotting import show, save
 
 input_grades = np.array([[4, 3, 3, 4, 5, 3],
                          [2.9, 5.1, 3, 3.1, 3, 3.9],
@@ -11,49 +12,52 @@ input_grades = np.array([[4, 3, 3, 4, 5, 3],
 random_grades = np.random.choice([1, 2, 3, 4, 5], size=(4, 6))
 
 input_weights = np.array([0.1,
-                            0.3,
-                            0.15,
-                            0.2,
-                            0.1,
-                            0.15]).T
+                          0.3,
+                          0.15,
+                          0.2,
+                          0.1,
+                          0.15]).T
+
 
 class WeightSensitivity():
-    def __init__(self,grades,weights) -> None:
+    def __init__(self, grades: np.ndarray, weights: np.ndarray) -> None:
         self.grades = grades
         self.start_weights = np.array([0.1,
-                            0.3,
-                            0.15,
-                            0.2,
-                            0.1,
-                            0.15]).T
+                                       0.3,
+                                       0.15,
+                                       0.2,
+                                       0.1,
+                                       0.15]).T
         self.weights = weights
         self.win_count = np.zeros(4)
         self.combinations = set()
-        self.weights_df = pd.DataFrame([self.final_grades],columns=['Winged Hovercraft','Rotating Wing','Folding Wing','Variable Skew Quadplane'])
-        
+        self.weights_df = pd.DataFrame([self.final_grades],
+                                       columns=['Winged Hovercraft', 'Rotating Wing', 'Folding Wing',
+                                                'VSQP'])
+
     @property
     def final_grades(self):
-        return sum((self.grades*self.weights).T)
-    
+        return np.dot(self.grades, self.weights)
+
     @property
     def winner(self):
         return np.argmax(self.final_grades)
-    
+
     def random_weigth_switch(self):
-        i_from = np.random.randint(0,6)
+        i_from = np.random.randint(0, 6)
         i_to = i_from
         while i_to == i_from:
-            i_to = np.random.randint(0,6)
+            i_to = np.random.randint(0, 6)
         boundary = 0.05
         lower_start = self.start_weights[i_from] - boundary
         higher_start = self.start_weights[i_to] + boundary
         if lower_start < self.weights[i_from] and self.weights[i_to] < higher_start:
             self.weights[i_from] -= 0.01
             self.weights[i_to] += 0.01
-        
-        
+
     def loop(self):
         count = 0
+        data = []
         for i in range(500000):
             self.random_weigth_switch()
             w = str(self.weights)
@@ -61,20 +65,26 @@ class WeightSensitivity():
                 count += 1
                 self.combinations.add(str(self.weights))
                 self.win_count[self.winner] += 1
-                newdata = pd.DataFrame([self.final_grades], columns=['Winged Hovercraft','Rotating Wing','Folding Wing','Variable Skew Quadplane'])
-                self.weights_df = pd.concat([self.weights_df, newdata])
+                data.append(self.final_grades)
+        self.weights_df = pd.DataFrame(data, columns=['Winged Hovercraft', 'Rotating Wing', 'Folding Wing', 'VSQP'])
 
-    def boxplot(self):
+    @show
+    @save
+    def plot_sensitivity(self) -> tuple[plt.Figure, plt.Axes]:
         # plt.rcParams["figure.figsize"] = [12, 8]
         # plt.rcParams["figure.autolayout"] = True
-        ax = self.weights_df[['Winged Hovercraft','Rotating Wing','Folding Wing','Variable Skew Quadplane']].plot(kind='kde', title='Grade distribution')
-        ax = plt.axhline(0, color='black')
-        # Display the plot
-        plt.savefig('sensitivity.pdf')
-        plt.show()
-
-
-
+        fig, ax = plt.subplots(figsize=(7, 5))
+        ax = self.weights_df[['Winged Hovercraft', 'Rotating Wing', 'Folding Wing', 'VSQP']].plot(
+            kind='kde',
+            ax=ax,
+            xlabel='Grades',
+            ylabel='Density',
+        )
+        # plt.axhline(0, color='black')
+        plt.ylim(bottom=0.01)
+        plt.xlabel('Scores')
+        plt.legend(loc='upper left')
+        return fig, ax
 
 
 if __name__ == '__main__':
@@ -84,6 +94,4 @@ if __name__ == '__main__':
     print(w.final_grades)
     print(w.win_count)
     print(len(w.combinations))
-    w.boxplot()
-
-    
+    w.plot_sensitivity()
