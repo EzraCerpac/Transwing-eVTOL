126738bafdfcc8c6827f142a8701c0e1cb640c31
tzoli6 <zozoturi@gmail.com>
2024-06-04 11:47:23 +0200

    fix

--- transition_model/transition_visualisation.py
+++ transition_model/transition_visualisation.py
@@ -60,38 +60,27 @@ class TransitionAnim:
 
         Xc, Yc, Zc = self.cylinder_data(0, .35, 0.5, 1, 0.1)
 
-        self.animation_ax.plot_surface(Zc,
-                                       Yc,
-                                       Xc,
-                                       alpha=1,
-                                       rstride=10,
-                                       cstride=5,
-                                       color="blue")
+        self.animation_ax.plot_surface(Zc, Yc, Xc, alpha=1, rstride=10, cstride=5, color="blue")
 
-        self.line1 = self.animation_ax.plot([self.A[0], self.B[0]],
-                                            [self.A[1], self.B[1]],
-                                            zs=[self.A[2], self.B[2]],
-                                            c=color)
-        self.line2 = self.animation_ax.plot([self.B[0], self.C[0]],
-                                            [self.B[1], self.C[1]],
-                                            zs=[self.B[2], self.C[2]],
-                                            c=color)
-        self.line3 = self.animation_ax.plot([self.C[0], self.D[0]],
-                                            [self.C[1], self.D[1]],
-                                            zs=[self.C[2], self.D[2]],
-                                            c=color)
-        self.line4 = self.animation_ax.plot([self.A[0], self.D[0]],
-                                            [self.A[1], self.D[1]],
-                                            zs=[self.A[2], self.D[2]],
-                                            c=color)
+        self.ax = self.animation_ax.quiver(0, 0, 0, self.axis[0, 0], self.axis[1, 0], self.axis[2, 0], arrow_length_ratio = 0.1, color='red')
+        
+        self.line1 = self.animation_ax.plot([self.A[0], self.B[0]], [self.A[1], self.B[1]], zs=[self.A[2], self.B[2]], c='purple')
+        self.line2 = self.animation_ax.plot([self.B[0], self.C[0]], [self.B[1], self.C[1]], zs=[self.B[2], self.C[2]], c='purple')
+        self.line3 = self.animation_ax.plot([self.C[0], self.D[0]], [self.C[1], self.D[1]], zs=[self.C[2], self.D[2]], c='purple')
+        self.line4 = self.animation_ax.plot([self.A[0], self.D[0]], [self.A[1], self.D[1]], zs=[self.A[2], self.D[2]], c='purple')
 
         self.vectors = []
 
         for r, v in zip(self.r_prop, self.v_prop):
-            tmp_v = r + v
-            self.vectors.append(
-                self.animation_ax.quiver(r[0, 0], r[1, 0], r[2, 0], v[0, 0],
-                                         v[1, 0], v[2, 0]))
+            tmp_v = r+v
+            self.vectors.append(self.animation_ax.quiver(r[0, 0], r[1, 0], r[2, 0], v[0, 0], v[1, 0], v[2, 0], arrow_length_ratio = 0.3, color='green'))
+
+        self.x_axis = self.animation_ax.plot([0, 1], [0, 0], zs=[0, 0], c='blue')
+        self.y_axis = self.animation_ax.plot([0, 0], [0, 1], zs=[0, 0], c='blue')
+        self.z_axis = self.animation_ax.plot([0, 0], [0, 0], zs=[0, 1], c='blue')
+
+        
+
 
         self.x_axis = self.animation_ax.plot([0, 1], [0, 0],
                                              zs=[0, 0],
@@ -127,22 +116,10 @@ class TransitionAnim:
         self.C = self.C_axis @ self.C
         self.D = self.C_axis @ self.D
 
-        self.line1 = self.animation_ax.plot([self.A[0], self.B[0]],
-                                            [self.A[1], self.B[1]],
-                                            zs=[self.A[2], self.B[2]],
-                                            c=color)
-        self.line2 = self.animation_ax.plot([self.B[0], self.C[0]],
-                                            [self.B[1], self.C[1]],
-                                            zs=[self.B[2], self.C[2]],
-                                            c=color)
-        self.line3 = self.animation_ax.plot([self.C[0], self.D[0]],
-                                            [self.C[1], self.D[1]],
-                                            zs=[self.C[2], self.D[2]],
-                                            c=color)
-        self.line4 = self.animation_ax.plot([self.A[0], self.D[0]],
-                                            [self.A[1], self.D[1]],
-                                            zs=[self.A[2], self.D[2]],
-                                            c=color)
+        self.line1 = self.animation_ax.plot([self.A[0], self.B[0]], [self.A[1], self.B[1]], zs=[self.A[2], self.B[2]], c='purple')
+        self.line2 = self.animation_ax.plot([self.B[0], self.C[0]], [self.B[1], self.C[1]], zs=[self.B[2], self.C[2]], c='purple')
+        self.line3 = self.animation_ax.plot([self.C[0], self.D[0]], [self.C[1], self.D[1]], zs=[self.C[2], self.D[2]], c='purple')
+        self.line4 = self.animation_ax.plot([self.A[0], self.D[0]], [self.A[1], self.D[1]], zs=[self.A[2], self.D[2]], c='purple')
 
         eff = []
 
@@ -184,12 +161,9 @@ class TransitionAnim:
         self.animation_ax = self.animation_fig.add_subplot(projection='3d')
         self.animation_ax.set_aspect('equal')
 
-        ani = animation.FuncAnimation(self.animation_fig,
-                                      self.draw_wing,
-                                      frames=n,
-                                      interval=50,
-                                      repeat=True,
-                                      init_func=self.init_animation)
+        ani = animation.FuncAnimation(self.animation_fig, self.draw_wing, frames=n, interval=50, repeat=True, init_func=self.init_animation)
+        ani.save(filename="./rotation.gif", writer="pillow", dpi=300)
+
 
         self.A = np.array([[0.1], [-0.75], [0]])
         self.B = np.array([[0.1], [0.25], [0]])
@@ -201,7 +175,8 @@ class TransitionAnim:
 
 #TODO: Move finctions into the class
 
-
+alpha = 0
+beta = 0
 def random_color():
     np.random.seed()
     rgbl = [np.random.random(), np.random.random(), np.random.random()]
@@ -217,7 +192,7 @@ C_y = np.array([[np.cos(np.pi / 2 - beta), 0,
                  np.cos(np.pi / 2 - beta)]])
 
 
-def solve():
+def optimize_axis():
     """Function calculating the rotational axis parameters for a given start and end location
     
     """
@@ -272,63 +247,6 @@ def solve():
         'abstol': 0.0001
     })
     print(G(0.5, 0.5))
-
-
-def update(frame_number):
-
-    alpha = 45 / 180 * np.pi  #0.852786 #35/180*np.pi
-    beta = 40 / 180 * np.pi  #0.0124301 #40/180*np.pi
-
-    C_z = np.array([[np.cos(alpha), -np.sin(alpha), 0],
-                    [np.sin(alpha), np.cos(alpha), 0], [0, 0, 1]])
-
-    C_y = np.array([[np.cos(np.pi / 2 - beta), 0,
-                     np.sin(np.pi / 2 - beta)], [0, 1, 0],
-                    [-np.sin(np.pi / 2 - beta), 0,
-                     np.cos(np.pi / 2 - beta)]])
-
-    x_axis = C_z @ C_y @ np.array([1, 0, 0])
-    y_axis = C_z @ C_y @ np.array([0, 1, 0])
-    z_axis = C_z @ C_y @ np.array([0, 0, 1])
-
-    #original axes
-    ax.plot([0, 1], [0, 0], zs=[0, 0], c='blue')
-    ax.plot([0, 0], [0, 1], zs=[0, 0], c='blue')
-    ax.plot([0, 0], [0, 0], zs=[0, 1], c='blue')
-
-    #ax.plot([0, x_axis[0]], [0, x_axis[1]], zs=[0, x_axis[2]], c='blue')
-    #ax.plot([0, y_axis[0]], [0, y_axis[1]], zs=[0, y_axis[2]], c='blue')
-    ax.plot([0, z_axis[0]], [0, z_axis[1]], zs=[0, z_axis[2]], c='red')
-    ax.scatter(0, 0, 0)
-
-    q = -120 / 180 * np.pi / 3  #100/180*np.pi/100
-
-    C_axis = np.cos(q) * np.eye(3, 3) + np.sin(q) * np.array(
-        [[0, -z_axis[2], z_axis[1]], [z_axis[2], 0, -z_axis[0]],
-         [-z_axis[1], z_axis[0], 0]]) + (1 - np.cos(q)) * z_axis * z_axis.T
-
-    A = np.array([[0.1], [-0.75], [0]])
-    B = np.array([[0.1], [0.25], [0]])
-    C = np.array([[-0.1], [0.25], [0]])
-    D = np.array([[-0.1], [-0.75], [0]])
-
-    for i in range(frame_number):
-        color = random_color()
-        A = C_axis @ A
-        B = C_axis @ B
-        C = C_axis @ C
-        D = C_axis @ D
-
-    ax.plot([A[0], B[0]], [A[1], B[1]], zs=[A[2], B[2]], c=color)
-    ax.plot([B[0], C[0]], [B[1], C[1]], zs=[B[2], C[2]], c=color)
-    ax.plot([C[0], D[0]], [C[1], D[1]], zs=[C[2], D[2]], c=color)
-    ax.plot([A[0], D[0]], [A[1], D[1]], zs=[A[2], D[2]], c=color)
-
-    ax.scatter(A[0], A[1], A[2], c=color)
-    ax.scatter(B[0], B[1], B[2], c=color)
-    ax.scatter(C[0], C[1], C[2], c=color)
-    ax.scatter(D[0], D[1], D[2], c=color)
-
 
 def run_animation():
     fig = plt.figure()
