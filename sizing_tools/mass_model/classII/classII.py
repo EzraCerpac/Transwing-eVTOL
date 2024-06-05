f780d13caae4553996fb1243f3d7ca03f81be1aa
KoenPijnacker <103990611+KoenPijnacker@users.noreply.github.com>
2024-06-05 09:25:50 +0200

    Class II improvements

--- sizing_tools/mass_model/classII/classII.py
+++ sizing_tools/mass_model/classII/classII.py
@@ -117,9 +117,9 @@ def concept_iteration(concepts: list[Aircraft]):
 if __name__ == '__main__':
     from data.concept_parameters.concepts import concept_C1_5, concept_C2_1, concept_C2_6, concept_C2_10
 
-    concept_iteration(
-        [concept_C1_5, concept_C2_1, concept_C2_6, concept_C2_10])
+    # concept_iteration(
+    #     [concept_C1_5, concept_C2_1, concept_C2_6, concept_C2_10])
 
     concept_iteration([
-        joby_s4,
+        concept_C2_1,
     ])
