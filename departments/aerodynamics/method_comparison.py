245a1277d1336e358d2a546c84c78080c5182475
ecerpac <ecerpac@student.tudelft.nl>
2024-06-07 13:08:23 +0200

    refactored AeroMethodComparison in method_comparison.py

--- departments/aerodynamics/method_comparison.py
+++ departments/aerodynamics/method_comparison.py
@@ -6,6 +6,7 @@ from matplotlib import pyplot as plt
 
 from aircraft_models import rot_wing
 from data.concept_parameters.aircraft import AC
+from departments.aerodynamics.aero import Aero
 from departments.aerodynamics.helper import OutputVal, label, AxisVal
 from sizing_tools.drag_model.class_II_drag import ClassIIDrag
 from utility.plotting import show
@@ -14,19 +15,26 @@ from utility.plotting import show
 class AeroMethodComparison:
     def __init__(self,
                  ac: AC,
-                 methods: dict[str, [Callable[[AC, np.ndarray], dict[str, any]]]],
+                 methods: tuple[str] = ('AeroBuildup', 'Class II', 'VLM'),
                  alpha: np.ndarray = np.linspace(-20, 20, 101)):
         self.ac = ac
-        self.methods = methods
         self.alpha = alpha
-        self.results: dict[str, dict[str, any]] = {k: {} for k in methods.keys()}
+        methods_map: dict[str, Callable[[], dict[str, any]]] = {
+            'AeroBuildup': Aero(ac=self.ac, alpha=self.alpha).get_aero_data,
+            'Class II': lambda: ClassIIDrag(ac=self.ac).aero_dict(alpha=self.alpha),
+            'VLM': lambda: vlm(ac=self.ac, alpha=self.alpha)
+        }
+        self.methods: dict[str, Callable[[], dict[str, any]]] = {k: methods_map[k] for k in methods}
+        self.results: dict[str, dict[str, any]] = {k: {} for k in self.methods.keys()}
 
     def run(self):
         for method_name, method in self.methods.items():
-            self.results[method_name] = method(self.ac, self.alpha)
+            self.results[method_name] = method()
 
     @show
     def plot_results(self, output_val: list[OutputVal] = None) -> tuple[plt.Figure, plt.Axes]:
+        if self.results is None:
+            self.run()
         if output_val is None:
             output_val = list(OutputVal)
         fig, axs = plt.subplots(1, len(output_val), figsize=(len(output_val) * 5, 8))
@@ -50,15 +58,8 @@ def vlm(ac: AC, alpha: np.ndarray) -> dict[str, any]:
     return {output_val.value: np.array([a[output_val.value] for a in data])
             for output_val in OutputVal}
 
-if __name__ == '__main__':
-    from data.concept_parameters.aircraft import AC
-    from departments.aerodynamics.aero import Aero
 
-    methods = {
-        'AeroBuildup': lambda ac, alpha: Aero(ac=ac, alpha=alpha).get_aero_data(),
-        'Class II': lambda ac, alpha: ClassIIDrag(ac=ac).aero_dict(alpha=alpha),
-        'VLM': vlm,
-    }
-    aero_method_comparison = AeroMethodComparison(ac=rot_wing, methods=methods)
+if __name__ == '__main__':
+    aero_method_comparison = AeroMethodComparison(ac=rot_wing)
     aero_method_comparison.run()
     aero_method_comparison.plot_results()
