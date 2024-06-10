8580690aa0250769962af65a23771f4cba166329
ecerpac <ecerpac@student.tudelft.nl>
2024-06-10 11:42:52 +0200

    added CL_max_at_trans_val() in Aero in aero.py

--- departments/aerodynamics/aero.py
+++ departments/aerodynamics/aero.py
@@ -1,6 +1,8 @@
 import aerosandbox as asb
 import aerosandbox.numpy as np
 
+TRANS_VALS = np.linspace(0, 1, 31)
+
 from data.concept_parameters.aircraft import AC
 
 
@@ -11,6 +13,8 @@ class Aero:
         self.alpha = alpha
         self.velocity = ac.data.cruise_velocity
         self.aero_data: dict = self.get_aero_data()
+        if self.ac.parametric_fn is not None:
+            self.trans_aero_data: dict = self.get_aero_over_trans_val(TRANS_VALS)
 
     def get_aero_data(self, include_wave_drag: bool = True, model_size: str = 'small') -> dict:
         return asb.AeroBuildup(
@@ -23,6 +27,19 @@ class Aero:
             include_wave_drag=include_wave_drag,
             model_size=model_size,
         ).run()
+
+    def get_aero_over_trans_val(self, include_wave_drag: bool = True, model_size: str = 'small', trans_vals: np.ndarray = np.linspace(0, 1, 31)) -> dict:
+        aero = [asb.AeroBuildup(
+            airplane=self.ac.parametric_fn(trans_val),
+            op_point=asb.OperatingPoint(
+                atmosphere=asb.Atmosphere(altitude=self.ac.data.cruise_altitude),
+                velocity=self.velocity,
+                alpha=self.alpha,
+            ),
+        ).run() for trans_val in trans_vals]
+        self.aero_data = {output_val: np.array([a[output_val] for a in aero])
+                     for output_val in aero[0].keys()}
+        return self.aero_data
 
     def CL(self, alpha: float) -> float:
         return np.interp(alpha, self.alpha, self.aero_data["CL"])
@@ -41,6 +58,10 @@ class Aero:
     def CL_max(self) -> float:
         return np.max(self.aero_data["CL"])
 
+    def CL_max_at_trans_val(self, trans_val: float) -> float:
+        cl_max = np.max(self.trans_aero_data["CL"], axis=1)
+        return np.interp(trans_val, TRANS_VALS, cl_max)
+
     @property
     def alpha_CL_max(self) -> float:
         return self.alpha[np.argmax(self.aero_data["CL"])]
@@ -51,8 +72,8 @@ class Aero:
 
 
 if __name__ == '__main__':
-    from aircraft_models import rot_wing
+    from aircraft_models import trans_wing
 
-    a = Aero(rot_wing)
-    print(a.CL_max)
-    print(a.alpha_CL_max)
+    a = Aero(trans_wing)
+    for i in np.linspace(0, 1, 11):
+        print(a.CL_max_at_trans_val(i))
