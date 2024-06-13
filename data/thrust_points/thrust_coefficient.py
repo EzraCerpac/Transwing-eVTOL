60934694c416e3630b5528c5598de2ee1409db37
ecerpac <ecerpac@student.tudelft.nl>
2024-06-13 11:28:37 +0200

    improved plotting and fixed issue in pitch_30.txt

--- data/thrust_points/thrust_coefficient.py
+++ data/thrust_points/thrust_coefficient.py
@@ -11,8 +11,7 @@ from utility.plotting import show
 DIR = Path(__file__).parent
 
 
-def thrust_coefficient(J: float | np.ndarray,
-                       pitch: float | np.ndarray) -> float | np.ndarray:
+def thrust_coefficient(J: float | np.ndarray, pitch: float | np.ndarray) -> float | np.ndarray:
     """
     Calculate the thrust coefficient for a propeller.
 
@@ -39,6 +38,29 @@ def plot_ct() -> tuple[plt.Figure, plt.Axes]:
     ax.set_ylabel("Thrust Coefficient, $C_T$")
     ax.set_ylim(0, .4)
     ax.legend(loc='upper right')
+    return fig, ax
+
+
+@show
+def plot_gradient() -> tuple[plt.Figure, plt.Axes]:
+    pitch = np.arange(20, 66, 5)
+    J = np.linspace(0.4, 6, 101)
+    pitch, J = np.meshgrid(pitch, J)
+    ct = thrust_coefficient(J, pitch)
+    fig, ax = plt.subplots(figsize=(10, 8))
+    p.contour(
+        J,
+        pitch,
+        ct,
+        levels=20,
+        colorbar_label="Thrust Coefficient, $C_T$",
+        linelabels_format=lambda x: f"{x:.2f}",
+        cmap='viridis',
+        z_log_scale=False,
+    )
+    # plt.clim(*np.array([-1, 1]) * np.max(np.abs(ct)))
+    plt.xlabel("Advance Ratio, $J$")
+    plt.ylabel("Pitch Angle, $\\theta$ (°)")
     return fig, ax
 
 
