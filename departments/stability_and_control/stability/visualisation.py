3d3497372d7f7bea4f3c59943b00431f9d83defe
ecerpac <ecerpac@student.tudelft.nl>
2024-06-19 12:22:31 +0200

    roll performance visualisation

--- departments/stability_and_control/stability/visualisation.py
+++ departments/stability_and_control/stability/visualisation.py
@@ -4,7 +4,7 @@ import aerosandbox.numpy as np
 
 from aircraft_models import rot_wing
 
-MODEL_SCALE = 2
+MODEL_SCALE = 5
 
 ac = rot_wing
 
@@ -22,7 +22,8 @@ def show_3D_longitudinal(response: ct.TimeResponseData, end_time=5):
     dyn.z_e = dyn.time * dyn.speed * np.sin(dyn.gamma)
     dyn.draw(ac.parametric, scale_vehicle_model=MODEL_SCALE)
 
-def show_3D_lateral(response: ct.TimeResponseData, end_time=5):
+def show_3D_lateral(response: ct.TimeResponseData, end_time=6):
+    import pyvista as pv
     end_index = np.argmin(np.abs(response.time - end_time))
     V = ac.data.cruise_velocity
     dyn = asb.DynamicsPointMass3DSpeedGammaTrack(
@@ -34,5 +35,22 @@ def show_3D_lateral(response: ct.TimeResponseData, end_time=5):
     )
     dyn.time = response.time[:end_index]
     dyn.x_e = dyn.time * dyn.speed
-    dyn.draw(ac.parametric, scale_vehicle_model=MODEL_SCALE)
+    # plotter = pv.Plotter()
+    plotter: pv.Plo = dyn.draw(
+        ac.parametric,
+        scale_vehicle_model=MODEL_SCALE,
+        n_vehicles_to_draw=8,
+        show=False,
+        draw_axes=False,
+        draw_global_axes=False,
+        draw_altitude_drape=False,
+        draw_ground_plane=False,
+        set_sky_background=False,
+        # draw_global_grid=False,
+    )
+    # plotter.camera.up = (0, 0, 0)
+    plotter.camera.Azimuth(180)
+    # plotter.camera.Elevation(00)
+    plotter.show(interactive=False, full_screen=True)
+    plotter.screenshot("roll3D", transparent_background=True, scale=5)
 
