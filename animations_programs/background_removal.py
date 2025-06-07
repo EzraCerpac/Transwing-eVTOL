4ac3490421e03700f889014d10bcfca703e2d6bb
Ezra Cerpac <e.cerpac@icloud.com>
2025-06-07 11:48:46 +0200

    Add all report files, documentation, and data

--- animations_programs/background_removal.py
+++ animations_programs/background_removal.py
@@ -4,8 +4,6 @@ import subprocess
 from pathlib import Path
 
 import cv2
-import imageio
-from PIL import Image
 from rembg import remove
 
 
@@ -23,23 +21,26 @@ def remove_background(input_path, output_path) -> bool:
         return False
     return True
 
+
 def remove_background_with_imagemagick(input_path: str, output_path: str, fuzz: str = '30%') -> bool:
     try:
         # Run the ImageMagick command
-        command = ['convert', input_path, '-fuzz', fuzz, '-fill', 'magenta', '-draw', 'color 0,0 floodfill', '-transparent', 'magenta', output_path]
-        subprocess.run(command, check=True)
-        print(f"Background removed successfully for {os.path.basename(input_path)}")
+        command = ['convert', input_path, '-fuzz', fuzz, '-fill', 'magenta', '-draw', 'color 0,0 floodfill',
+                   '-transparent', 'magenta', output_path]
+        subprocess.Popen(command)
+        print(f"Background removal started for {os.path.basename(input_path)}")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred processing {os.path.basename(input_path)}: {e}")
         return False
     return True
 
+
 def process_image(paths) -> bool:
-    # return remove_background(*paths)
-    return remove_background_with_imagemagick(*paths)
+    return remove_background(*paths)
+    # return remove_background_with_imagemagick(*paths)
 
 
-def remove_background_from_images(input_dir, output_dir):
+def remove_background_from_images(input_dir, output_dir, crop=False):
     # Ensure output directory exists
     os.makedirs(output_dir, exist_ok=True)
 
@@ -57,6 +58,43 @@ def remove_background_from_images(input_dir, output_dir):
     else:
         print("Some images were not processed successfully.")
 
+    if crop:
+        crop_images(output_dir, output_dir)
+
+
+def crop_images(input_dir, output_dir):
+    # Ensure output directory exists
+    os.makedirs(output_dir, exist_ok=True)
+    for filename in os.listdir(input_dir):
+        if filename.endswith('.png'):
+            # Read the image
+            image = cv2.imread(os.path.join(input_dir, filename), -1)
+
+            pre_crop = 100
+            h, w = image.shape[:2]
+            image = image[pre_crop:h-pre_crop, pre_crop:w-pre_crop]
+
+            # Convert the image to grayscale
+            grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            # Apply a threshold to get a binary image
+            _, binary = cv2.threshold(grey, 20, 255, cv2.THRESH_BINARY)
+
+            # Find contours
+            contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            # Calculate the bounding rectangle of all contours
+            x_min = min([cv2.boundingRect(contour)[0] for contour in contours])
+            y_min = min([cv2.boundingRect(contour)[1] for contour in contours])
+            x_max = max([cv2.boundingRect(contour)[0] + cv2.boundingRect(contour)[2] for contour in contours])
+            y_max = max([cv2.boundingRect(contour)[1] + cv2.boundingRect(contour)[3] for contour in contours])
+
+            # Crop the image using the bounding rectangle
+            cropped_image = image[y_min:y_max, x_min:x_max]
+
+            # Save the cropped image with the alpha channel
+            cv2.imwrite(os.path.join(output_dir, filename), cropped_image, [cv2.IMWRITE_PNG_COMPRESSION, 9])
+            print(f"Image {filename} cropped successfully")
 
 
 if __name__ == '__main__':
@@ -64,5 +102,13 @@ if __name__ == '__main__':
     output_directory = Path(__file__).parent / 'output_imgs'
     output_file = Path(__file__).parent / 'output.gif'
 
-    remove_background_from_images(input_directory, output_directory)
+    # input_directory = "/Users/ezracerpac/PycharmProjects/Transwing-eVTOL/data/flight_data/ac_jpgs"
+    # output_directory = "/Users/ezracerpac/PycharmProjects/Transwing-eVTOL/data/flight_data/ac_pngs"
+    # output2_directory = "/Users/ezracerpac/PycharmProjects/Transwing-eVTOL/data/flight_data/ac_pngs2"
+
+    input_directory = Path(__file__).parent / 'tims_renders'
+    output_directory = Path(__file__).parent / 'tims_renders_output'
+
+    remove_background_from_images(input_directory, output_directory, crop=False)
+    # crop_images(output_directory, output2_directory)
     # create_gif(output_directory, output_file, duration=0.1)
--- data/flight_data/mission_profile_animation.py
+++ data/flight_data/mission_profile_animation.py
@@ -3,26 +3,58 @@ from pathlib import Path
 
 import matplotlib.animation as animation
 import numpy as np
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, image as mpimg
 
 from data.flight_data.mission_data import mission_data
 
-AIRPLANE_DIR = Path(__file__).parent / 'ac_jpgs'
+# AIRPLANE_DIR = Path(__file__).parent / 'ac_jpgs'
+AIRPLANE_DIR = Path(__file__).parent / 'ac_pngs'
 JPEG_FILES = {trans_val: AIRPLANE_DIR / f'frame_{i_int:03d}.jpeg'
               for i_int, trans_val in enumerate(np.linspace(0, 1, 301))}
-data = mission_data
+
+# alter the data such that the cruise time is much shorter
+mission_data = deepcopy(mission_data)
 
 
-def draw_airplane(ax: plt.Axes, position: [float, float], trans_val: float, scale: float = .1) -> plt.Axes:
+def scale_segment(mission_data, segment: str, scale: float, variable: str = 'time'):
+    start_index = mission_data['segment'].eq(segment).idxmax()
+    end_index = mission_data['segment'].eq(segment).idxmax() + mission_data['segment'].eq(segment).sum()
+    start_time = mission_data[variable].iloc[start_index]
+    delta_time = (mission_data[variable].iloc[end_index] - mission_data[variable].iloc[start_index]) * scale
+    new_end_time = start_time + delta_time
+    old_end_time = mission_data[variable].iloc[end_index]
+    mission_data.loc[start_index:end_index, variable] = np.linspace(start_time, new_end_time, end_index - start_index + 1)
+    mission_data.loc[end_index + 1:, variable] = mission_data[variable].iloc[end_index + 1:] + (new_end_time - old_end_time)
+    return mission_data
+
+
+mission_data = scale_segment(mission_data, 'Vertical Climb', 2)
+mission_data = scale_segment(mission_data, 'First Transition', 2)
+mission_data = scale_segment(mission_data, 'First Transition', 2, variable='x')
+mission_data = scale_segment(mission_data, 'Climb', .2)
+mission_data = scale_segment(mission_data, 'Cruise', 0.1)
+mission_data = scale_segment(mission_data, 'Cruise', 0.5, variable='x')
+mission_data = scale_segment(mission_data, 'Descend', .2)
+mission_data = scale_segment(mission_data, 'Descend', 1.5, variable='x')
+mission_data = scale_segment(mission_data, 'Second Transition', 1.5)
+mission_data = scale_segment(mission_data, 'Second Transition', 1.5, variable='x')
+mission_data = scale_segment(mission_data, 'Vertical Descend', 2)
+
+
+def draw_airplane(ax: plt.Axes, position: [float, float], trans_val: float, scale: float = .5,
+                  invert: bool = False) -> plt.Axes:
     '''Display the airplane jpeg at a given position, scale and transformation value.'''
     # Load the jpeg file for the closest transformation value
     img_index = np.argmin([np.abs(trans_val - img_trans_file) for img_trans_file in JPEG_FILES.keys()])
-    img_file = AIRPLANE_DIR / f'frame_{img_index:03d}.jpeg'
+    img_file = AIRPLANE_DIR / f'frame_{img_index:03d}.png'
     try:
         img = plt.imread(img_file)
     except FileNotFoundError:
         print(f'Error: {img_file} not found.')
         return ax
+
+    if invert:
+        img = np.fliplr(img)
 
     width, height = ax.get_xlim()[1] - ax.get_xlim()[0], ax.get_ylim()[1] - ax.get_ylim()[0]
     aspect = width / height
@@ -43,41 +75,52 @@ def draw_airplane(ax: plt.Axes, position: [float, float], trans_val: float, scal
     return ax
 
 
-def animate_mission_profile(n_frames: int = len(data), duration: float = 10):
+def animate_mission_profile(duration: float = 10, fps: int = 30):
     global ax
-    mission_data = data.iloc[::len(data) // n_frames]
-    fps = n_frames / duration
+    # Create a time array that represents the time steps at which you want to animate the mission
+    time = np.linspace(mission_data['time'].min(), mission_data['time'].max(), int(duration * fps))
 
-    fig, ax = plt.subplots(figsize=(10, 10))
+    # Interpolate the mission_data to get the altitude, distance, and transformation value at each time step
+    altitude = np.interp(time, mission_data['time'], mission_data['altitude'])
+    distance = np.interp(time, mission_data['time'], mission_data['x'])
+    trans_val = np.interp(time, mission_data['time'], mission_data['trans val'])
+
+    fig, ax = plt.subplots(figsize=(20, 20))
+    background = mpimg.imread(Path(__file__).parent / 'background.jpg')
+    aspect = background.shape[1] / background.shape[0]
     offset_x, offset_y = 0.05 * mission_data['x'].max(), 0.05 * mission_data['altitude'].max()
 
     def update(frame):
         global ax
         ax.clear()
+        ax.axis('off')
+        ax.set_aspect(aspect, adjustable='datalim')
         ax.set_xlim(-offset_x, mission_data['x'].max() + offset_x)
-        ax.set_ylim(-offset_y, mission_data['altitude'].max() + offset_y)
-        ax.set_xlabel('Distance, $x$ [km]')
-        ax.set_ylabel('Altitude, $h$ [m]')
+        v_scale = 0.5
+        ax.set_ylim(-offset_y, mission_data['altitude'].max() / v_scale + offset_y)
+        # ax.set_xlabel('Distance, $x$ [km]')
+        # ax.set_ylabel('Altitude, $h$ [m]')
 
         # Get the current altitude and distance
-        altitude = mission_data['altitude'].iloc[frame]
-        distance = mission_data['x'].iloc[frame]
-        trans_val = mission_data['trans val'].iloc[frame]
+        current_altitude = altitude[frame]
+        current_distance = distance[frame]
+        current_trans_val = trans_val[frame]
 
-        ax.plot(mission_data['x'], mission_data['altitude'], label='Mission Profile', color='red')
+        ax.imshow(background, extent=(ax.get_xlim()[0] - offset_x, ax.get_xlim()[1] + offset_x, ax.get_ylim()[0], ax.get_ylim()[1] * (v_scale + .1)))
+        ax.plot(distance, altitude, '--', label='Mission Profile', color='blue', alpha=0.5, linewidth=5)
 
         # Draw the airplane at the current altitude and distance
-        ax = draw_airplane(ax, [distance, altitude], trans_val, scale=.5)
+        ax = draw_airplane(ax, [current_distance, current_altitude], current_trans_val, scale=.5, invert=True)
 
     # Create the animation
-    anim = animation.FuncAnimation(fig, update, frames=len(mission_data), repeat=False)
+    anim = animation.FuncAnimation(fig, update, frames=len(time), repeat=False)
 
     # Show the animation
     anim.save('mission_profile_animation.mp4', writer='ffmpeg', fps=fps)
 
 
 if __name__ == '__main__':
-    animate_mission_profile(20)
+    animate_mission_profile(duration=20, fps=20)
 
     # fig, ax = plt.subplots(figsize=(10, 10))
     # frame = 20
